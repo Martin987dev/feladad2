71c87314a3e7566b8d44efaaa447a64c865f81e9
Lempner Martin <LempnerM@kkszki.local>
2025-09-22 07:45:38 +0200

    kons

--- Connect.cs
+++ Connect.cs
@@ -18,12 +18,12 @@ namespace ConsoleApp2
         private string _password;
         private string ConnectionString;
 
-        public Connect(string database, string user, string password)
+        public Connect(string database)
         {
             _host = "localhost";
             _database = database;
-            _user = user;
-            _password = password;
+            _user = "root";
+            _password = "password";
 
             ConnectionString = $"SERVER={_host};DATABASE={_database};UID={_user};PASSWORD={_password},SslMode=None";
 
